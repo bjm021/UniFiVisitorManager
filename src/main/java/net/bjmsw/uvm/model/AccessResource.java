ce482f957f7d020a6bbb6e22d51a6ff4b4dafc24
Benjamin J. Meyer <benmeyer@outlook.de>
2026-03-01 21:52:48 +0100

    Mini updates

--- src/main/java/net/bjmsw/uvm/model/AccessResource.java
+++ src/main/java/net/bjmsw/uvm/model/AccessResource.java
@@ -1,4 +1,8 @@
 package net.bjmsw.uvm.model;
 
+/**
+ * This represents a UniFi Access Resource
+ * That being a door or door group, basically a resource that can be used in an Access Policy
+ */
 public class AccessResource {
 }
